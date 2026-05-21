ad6aff081e352efafe2f5a279c1c1a1b83a98144
Samey <sam.afzali@outlook.de>
2026-05-21 20:48:40 +0200

    feat: NGSI-LD Parking Server Implementiert

---
+++ ngsi-server/src/main/java/de/smartcity/ngsiserver/NgsiServerApplication.java
@@ -0,0 +1,13 @@
+package de.smartcity.ngsiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgsiServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NgsiServerApplication.class, args);
+    }
+
+}
---
+++ ngsi-server/src/test/java/de/smartcity/ngsiserver/NgsiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.smartcity.ngsiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgsiServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
